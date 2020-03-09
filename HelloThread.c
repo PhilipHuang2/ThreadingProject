0a218ab797b0f6a9f04dfeb64cafaf302daaa5a6
Philip Huang <philiphuang2@gmail.com>
2020-03-09 14:56:06 -0700

    added buffer

--- HelloThread.c
+++ HelloThread.c
@@ -14,21 +14,21 @@ typedef struct {
 	int item_count;
 } Thread;
 
-// typedef struct {
-// 	int *buf; /* Buffer array */
-// 	int n; /* Maximum number of slots*/
-// 	int front; /* buf[(front+1)%n] is first item */
-// 	int read;  /* buf[rear%n] is last item */
-// 	sem_t mutex; // protecxts accesses to buf
-// 	sem_t slots; // Counts available slots
-// 	sem_t items; // Counts available items
-// } sbuf_t;
+typedef struct {
+	int *buf; /* Buffer array */
+	int n; /* Maximum number of slots*/
+	int front; /* buf[(front+1)%n] is first item */
+	int rear;  /* buf[rear%n] is last item */
+	sem_t mutex; // protecxts accesses to buf
+	sem_t slots; // Counts available slots
+	sem_t items; // Counts available items
+} sbuf_t;
 
-// // Buffer Functions
-// void sbuf_init(sbuf_t *sp, int n);
-// void sbuf_deinit(sbuf_t *sp);
-// void sbuf_insert(sbuf_t *sp, int item);
-// int sbuf_remove(sbuf_t *sp);
+// Buffer Functions
+void sbuf_init(sbuf_t *sp, int n);
+void sbuf_deinit(sbuf_t *sp);
+void sbuf_insert(sbuf_t *sp, int item);
+int sbuf_remove(sbuf_t *sp);
 
 // Thread Functions
 void*producer(void*vargp);
@@ -38,49 +38,55 @@ void*consumer(void*amount);
 void createProducerThreads(int amount, int item_count);
 void createConsumerThreads(int amount, int item_count);
 
+sbuf_t *buffer;
 int main()
 {
+
+	sbuf_init(buffer,8);
 	createProducerThreads(10, 10);
 	createConsumerThreads(10, 100);
 	exit(0);
 }
 
 // Buffer Functions
-// void sbuf_init(sbuf_t *sp, int n)
-// {
-// 	sp->buf = calloc(n, sizeof(int));
-// 	sp->n = n;
-// 	sp->front = sp->rear = 0;
-// 	Sem_init(&sp->mutex, 0, 1);
-// 	Sem_init(&sp->slots, 0, n);
-// 	Sem_init(&sp->items, 0, 0);
-// }
+void sbuf_init(sbuf_t *sp, int n)
+{
+	printf("Line 1.\n");
+	sp->buf = (int*)malloc(n *  sizeof(int));
+	printf("Line 1.\n");
+	sp->n = n;
+	printf("Line 1.\n");
+	sp->front = sp->rear = 0; 	printf("Line 1.\n");
+	sem_init(&sp->mutex, 0, 1); printf("Line 1.\n");
+	sem_init(&sp->slots, 0, n); printf("Line 1.\n");
+	sem_init(&sp->items, 0, 0); printf("Line 1.\n");
+}
 
-// void sbuf_deinit(sbuf_t *sp)
-// {
-// 	Free(sp->buff);
-// }
+void sbuf_deinit(sbuf_t *sp)
+{
+	free(sp->buf);
+}
 
-// void sbuf_insert(sbuf_t *sp, int item)
-// {
-// 	P(&sp->slots);
-// 	P(&sp->mutex);
-// 	sp->buf[(++sp->rear)%(sp->n)] = item;
-// 	V(&sp->mutex);
-// 	V(&sp->items);
+void sbuf_insert(sbuf_t *sp, int item)
+{
+	sem_wait(&sp->slots);
+	sem_wait(&sp->mutex);
+	sp->buf[(++sp->rear)%(sp->n)] = item;
+	sem_post(&sp->mutex);
+	sem_post(&sp->items);
 
-// }
+}
 
-// int sbuf_remove(sbuf_t *sp)
-// {
-// 	int item;
-// 	P(&sp->items);
-// 	P(&sp->mutex);
-// 	item = sp->buf[(++sp->front)%(sp->n)];
-// 	V(&sp->mutex);
-// 	V(&sp->slots);
-// 	return item;
-// }
+int sbuf_remove(sbuf_t *sp)
+{
+	int item;
+	sem_wait(&sp->items);
+	sem_wait(&sp->mutex);
+	item = sp->buf[(++sp->front)%(sp->n)];
+	sem_post(&sp->mutex);
+	sem_post(&sp->slots);
+	return item;
+}
 
 
 
@@ -111,13 +117,15 @@ void*consumer(void*amount)
 void createProducerThreads(int amount, int item_count)
 {
 	pthread_t * array = malloc(amount* sizeof(pthread_t));
+	Thread * threadArray = malloc(amount * sizeof(Thread));
 	int count = 0;
 	while(count < amount)
 	{
-		Thread week = {.item_count = item_count, .self_id = count};
-		void* dummy = &week;
+		threadArray[count].item_count = item_count;
+		threadArray[count].self_id = count; 
+		void* dummy = &threadArray[count];
 		pthread_create(&array[count],NULL,producer,dummy);
-		
+		pthread_join(array[count], NULL);
 		count++;
 	}
 	pthread_join(array[9], NULL);
@@ -126,13 +134,16 @@ void createProducerThreads(int amount, int item_count)
 void createConsumerThreads(int amount, int item_count)
 {
 	pthread_t * array = malloc(amount* sizeof(pthread_t));
+	Thread * threadArray = malloc(amount * sizeof(Thread));
 	int count = 0;
 	while(count < amount)
 	{
-		Thread week = {.item_count = item_count, .self_id = count};
-		void* dummy = &week;
+		threadArray[count].item_count = item_count;
+		threadArray[count].self_id = count; 
+		void* dummy = &threadArray[count];
 		pthread_create(&array[count],NULL,consumer,dummy);
-		pthread_join(array[count], NULL);
+		pthread_join(array[count], NULL); // comment out to detach functions
 		count++;
 	}
+	pthread_join(array[9], NULL);
 }
